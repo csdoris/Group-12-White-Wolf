3a86c34f23e93211f3185948844fb1c5a37d7a53
jteo97 <justinteo97@gmail.com>
2021-04-28 15:45:11 +1200

    fixed using frontend ev vars to retrieving from backend

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,14 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import GoogleMaps from '../Components/GoogleMaps';
 import SideNav from '../Components/Sidebar';
+import { Loader } from '@googlemaps/js-api-loader';
+import axios from 'axios';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function Home() {
-    return (
-        <div>
-            <SideNav />
-            <GoogleMaps />
-        </div>
-    );
+    const [keyObtained, setKeyObtained] = useState(false);
+
+    useEffect(() => {
+        axios
+            .get('/api/apikeys/googlekey')
+            .then(function (response) {
+                const loader = new Loader({
+                    apiKey: response.data.googleAPIKey,
+                    libraries: ['places'],
+                });
+                loader.load().then(() => {
+                    setKeyObtained(true);
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, []);
+
+    if (keyObtained) {
+        return (
+            <div>
+                <SideNav />
+                <GoogleMaps />
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <SideNav />
+                <CircularProgress />
+            </div>
+        );
+    }
 }
 
 export default Home;
