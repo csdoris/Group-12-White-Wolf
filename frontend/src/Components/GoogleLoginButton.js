1a1b9eff84a0d5011c84b381d268495878cdf8d8
Emily Yang <zyan225@aucklanduni.ac.nz>
2021-04-29 16:39:45 +1200

    [FRONT-014] handle google login error when cookie is blocked

--- frontend/src/Components/GoogleLoginButton.js
+++ frontend/src/Components/GoogleLoginButton.js
@@ -4,8 +4,17 @@ import { GoogleLogin } from 'react-google-login';
 import styles from '../Styles/GoogleLoginButton.module.css'
 
 export default function GoogleLoginButton({ setGoogleToken }) {
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [enable, setEnable] = useState(true);
+
     const responseGoogle = (response) => {
-        console.log(response);
+        if (response.error === "idpiframe_initialization_failed") {
+            setEnable(false);
+            setErrorMsg("Enable cookie to login using Google");
+        }
+        else {
+            setEnable(true);
+        }
     }
 
     const handleSuccess = async googleData => {
@@ -13,14 +22,17 @@ export default function GoogleLoginButton({ setGoogleToken }) {
     }
 
     return (
-         <GoogleLogin
-            className={styles.loginButton}
-            clientId="641864577175-63nlpegl4tdh39gabri4n3nk0o40sl54.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={handleSuccess}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-            buttonText={"Login using Google"}
-        />
+        <>
+            <GoogleLogin
+                className={styles.loginButton}
+                clientId="641864577175-63nlpegl4tdh39gabri4n3nk0o40sl54.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={handleSuccess}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+                buttonText={enable ? "Login using Google" : errorMsg}
+                disabled={!enable}
+            />
+        </>
     );
 }
