d451c714d4c8d9e47e9cfc985f837c3ec8e37231
Annie Chau <annithinggoes@gmail.com>
2021-05-04 00:25:38 +1200

    [FRONT-012] Set up app context for plans

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,37 +8,49 @@ import useToken from './hooks/useToken'
 import PrivateRoute from './Components/PrivateRoute'
 
 import './App.css';
+import { AppContext } from './AppContextProvider';
 
 function App() {
 
-    const {token, setToken} = useToken();
-    
+    const [plans, setPlans] = useState([]);
+    const value = { plans, setPlans };
+
+    const { token, setToken } = useToken();
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+
     function handleLoginData(data) {
         console.log(data);
+        setUsername(data.name);
+        setEmail(data.email);
         setToken(data.token);
     }
 
     return (
         <>
-            <BrowserRouter>
-                <Switch> 
-                    <Route exact path="/login">
-                        <button onClick={gettee}>click me to get</button>
-                        <LoginPage setData={handleLoginData}/>
-                    </Route>
-                    <Route exact path="/signup">
-                        <SignupPage />
-                    </Route>
-                    <PrivateRoute exact path="/home" 
-                        component={Home} 
-                        authenticated={token ? true : false} 
-                        token={token}
-                    />
-                    <Route path="/">
-                        <Redirect to="/home" />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
+            <AppContext.Provider value={value}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/login">
+                            <button onClick={gettee}>click me to get</button>
+                            <LoginPage setData={handleLoginData} />
+                        </Route>
+                        <Route exact path="/signup">
+                            <SignupPage />
+                        </Route>
+                        <PrivateRoute exact path="/home"
+                            component={Home}
+                            authenticated={token ? true : false}
+                            username={username}
+                            email={email}
+                            token={token}
+                        />
+                        <Route path="/">
+                            <Redirect to="/home" />
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </AppContext.Provider>
         </>
     );
 }
@@ -46,7 +58,7 @@ function App() {
 // example post to backend
 const gettee = async () => {
     console.log('clicked');
-    let weatherInfo = await FetchWeatherInfo('Auckland' , 36.8509, 174.7645);
+    let weatherInfo = await FetchWeatherInfo('Auckland', 36.8509, 174.7645);
     console.log(weatherInfo);
 };
 
